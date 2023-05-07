4818b83ff7840ab37aadbf772277c40452a2a401
Echo Wang <ywang623@my.bcit.ca>
2023-05-07 14:59:07 -0700

    Added nutritional goal calculation and profile page

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 require("./utils.js");
 
-require('dotenv').config();
-const express = require('express');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const bcrypt = require('bcrypt');
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const bcrypt = require("bcrypt");
 const saltRounds = 12;
 const port = process.env.PORT || 3030;
 const app = express();
 const Joi = require("joi");
 const cors = require("cors");
 
-const expireTime = 1 * 60 * 60 * 1000; 
+const expireTime = 1 * 60 * 60 * 1000;
+
+const goalCalculation = require("./goalCalculation.js");
 
 /* secret information section */
 const mongodb_host = process.env.MONGODB_HOST;
@@ -23,96 +25,101 @@ const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
-var {database} = include('databaseConnection');
+var { database } = include("databaseConnection");
 
-const userCollection = database.db(mongodb_database).collection('users');
+const userCollection = database.db(mongodb_database).collection("users");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/img", express.static("./public/images"));
 app.use("/css", express.static("./public/css"));
 
-
-
 var mongoStore = MongoStore.create({
-	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
-	crypto: {
-		secret: mongodb_session_secret
-	}
-})
+  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+  crypto: {
+    secret: mongodb_session_secret,
+  },
+});
 
-app.use(session({ 
+app.use(
+  session({
     secret: node_session_secret,
-	store: mongoStore, //default is memory store 
-	saveUninitialized: false, 
-	resave: true
-}
-));
+    store: mongoStore, //default is memory store
+    saveUninitialized: false,
+    resave: true,
+  })
+);
 
-app.get('/', (req,res) => {
+app.get("/", (req, res) => {
   var username = req.query.user;
   if (!req.session.authenticated) {
     res.render("index_before_login");
     return;
-  } else{
-    res.render("index_after_login", {username: req.session.username});
+  } else {
+    res.render("index_after_login", { username: req.session.username });
   }
 });
 
-
-app.get('/signup', (req,res) => {
+app.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-app.post('/submitUser',async(req,res)=>{
+app.post("/submitUser", async (req, res) => {
   var username = req.body.username;
   var email = req.body.email;
   var password = req.body.password;
-  if(!username){
-    res.render("signup_error", {error: "Name"});
+  if (!username) {
+    res.render("signup_error", { error: "Name" });
   }
-  if(!email){
-    res.render("signup_error", {error: "Email"});
+  if (!email) {
+    res.render("signup_error", { error: "Email" });
   }
-  if(!password){
-    res.render("signup_error", {error:"Password"});
+  if (!password) {
+    res.render("signup_error", { error: "Password" });
   }
-  const schema = Joi.object(
-		{
-			username: Joi.string().alphanum().max(20).required(),
-      email: Joi.string().max(30).required(),
-			password: Joi.string().max(20).required()
-		});
-	
-	const validationResult = schema.validate({username, email, password});
-	if (validationResult.error != null) {
-	   console.log(validationResult.error);
-	   res.redirect("/signup");
-	   return;
-   }
-    var hashedPassword = await bcrypt.hash(password, saltRounds);
-    await userCollection.insertOne({username: username, email:email, password: hashedPassword, user_type: "user"});
+  const schema = Joi.object({
+    username: Joi.string().alphanum().max(20).required(),
+    email: Joi.string().max(30).required(),
+    password: Joi.string().max(20).required(),
+  });
+
+  const validationResult = schema.validate({ username, email, password });
+  if (validationResult.error != null) {
+    console.log(validationResult.error);
+    res.redirect("/signup");
+    return;
+  }
+  var hashedPassword = await bcrypt.hash(password, saltRounds);
+  await userCollection.insertOne({
+    username: username,
+    email: email,
+    password: hashedPassword,
+    user_type: "user",
+  });
 
   req.session.authenticated = true;
   req.session.username = username;
   req.session.cookie.maxAge = expireTime;
-  res.redirect('/home');
+  res.redirect("/home");
   return;
-})
+});
 
-app.get('/login', (req,res) => {
+app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.post('/loggingIn', async (req,res) => {
+app.post("/loggingIn", async (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
-	const result = await userCollection.find({email: email}).project({email: 1, username: 1, password: 1, user_type: 1, _id: 1}).toArray();
+  const result = await userCollection
+    .find({ email: email })
+    .project({ email: 1, username: 1, password: 1, user_type: 1, _id: 1 })
+    .toArray();
   console.log(result);
   if (result.length != 1) {
-    res.render("login_error", {error: "email"});
+    res.render("login_error", { error: "email" });
     return;
   }
   if (await bcrypt.compare(password, result[0].password)) {
@@ -121,37 +128,67 @@ app.post('/loggingIn', async (req,res) => {
     req.session.username = result[0].username;
     req.session.user_type = result[0].user_type;
     req.session.cookie.maxAge = expireTime;
-    res.redirect('/members');
+    res.redirect("/members");
     return;
-  }
-  else {
+  } else {
     console.log("incorrect password");
-    res.render("login_error", {error: "password"});
+    res.render("login_error", { error: "password" });
     return;
   }
 });
 
-app.get('/home', (req,res) => {
+app.get("/home", (req, res) => {
   if (!req.session.authenticated) {
-    res.redirect('/');
-} else {
-  res.render("home", {username: req.session.username})
-}
+    res.redirect("/");
+  } else {
+    res.render("home", { username: req.session.username });
+  }
 });
 
-app.get('/logout', (req,res) => {
-	req.session.destroy();
-  res.redirect('/');
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 });
 
+app.get("/signup_profile", (req, res) => {
+  res.render("signup_profile");
+});
+
+app.post("/profile", (req, res) => {
+  const { sex, age, height, weight, activity, goal } = req.body;
+  const BMR = goalCalculation.calculateBMR(sex, weight, height, age);
+  const calorieNeeds = goalCalculation.calculateCalorieNeeds(
+    activity,
+    BMR,
+    goal
+  );
+  const { protein, fat, carbs } = goalCalculation.calculateMacronutrients(
+    weight,
+    calorieNeeds,
+    goal
+  );
+
+  res.render("profile", {
+    sex,
+    age,
+    height,
+    weight,
+    activity,
+    goal,
+    calorieNeeds,
+    protein,
+    fat,
+    carbs,
+  });
+});
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("*", (req,res) => {
-	res.status(404);
-	res.render("404");
-})
+app.get("*", (req, res) => {
+  res.status(404);
+  res.render("404");
+});
 
 app.listen(port, () => {
-	console.log("Node application listening on port "+port);
-}); 
+  console.log("Node application listening on port " + port);
+});

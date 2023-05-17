345deee5e76872a15a017c883b5ec45855f87e94
IrisWRX <wxu64@my.bcit.ca>
2023-05-16 21:23:11 -0700

    Persisted the click event data into the database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("./utils.js");
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
+const mongodb = require("mongodb");
 const MongoStore = require("connect-mongo");
 const bcrypt = require("bcrypt");
 const saltRounds = 12;
@@ -68,6 +69,7 @@ createSearchArray();
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // for parsing application/json
 
 const goalCalculation = require("./public/js/goalCalculation.js");
 
@@ -628,14 +630,59 @@ app.get("/menu/:restaurantName", async (req, res) => {
       menuCollection.find({ restaurant: restaurantName }).toArray(),
     ]);
 
-    console.log(restaurant),
-      console.log(menu),
-      res.render("menu", { restaurant, menu });
+    const username = req.session.username;
+
+    res.render("menu", { restaurant, menu, username });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
   }
 });
+
+app.post("/addItem", async (req, res) => {
+  console.log("req.body:", req.body);
+
+  const itemId = req.body.itemId;
+  const username = req.session.username;
+  console.log("itemId:", itemId); 
+  console.log("username:", username);
+
+  const userCollection = database.db(mongodb_database).collection("users");
+  const menuCollection = database.db(mongodb_database).collection("fastfoodnutrition");
+
+  try {
+    const user = await userCollection.findOne({ username: username });
+    const userId = user._id;
+    
+    if (!mongodb.ObjectId.isValid(itemId) || !mongodb.ObjectId.isValid(userId)) {
+      console.error("Invalid itemId or userId");
+      return res.json({ success: false });
+    }
+
+    const item = await menuCollection.findOne({
+      _id: new mongodb.ObjectId(itemId),
+    });
+    console.log("item:", item); 
+
+    const updateResult = await userCollection.updateOne(
+      { _id: new mongodb.ObjectId(userId) },
+      { $push: { trayItems: item } }
+    );
+    console.log("updateResult:", updateResult); 
+
+    if (updateResult.matchedCount > 0) {
+      const updatedUser = await userCollection.findOne({ _id: new mongodb.ObjectId(userId) });
+      const trayItemCount = updatedUser.trayItems.length;
+      res.json({ success: true, trayItemCount: trayItemCount });
+    } else {
+      res.json({ success: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false });
+  }
+});
+
 
 app.get("/filter", async (req, res) => {
   const checkedFilters = Array.isArray(req.query.filter)
@@ -737,10 +784,6 @@ app.get("/item/:restaurant/:item", async (req, res) => {
     protein_goal: goals[0].protein,
     fat_goal: goals[0].fat,
   });
-});
-
-app.get('/mytray', function(req, res) {
-  res.render('mytray');
 });
 
 app.get("/logout", (req, res) => {

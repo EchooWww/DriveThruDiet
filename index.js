3b63244d77293210a7a51b5041b70f5f99cdb3d1
IrisWRX <wxu64@my.bcit.ca>
2023-05-17 12:27:50 -0700

    minor style changes

--- index.js
+++ index.js
@@ -690,20 +690,20 @@ app.post("/addItem", async (req, res) => {
   }
 });
 
-app.get("/trayItemCount", async (req, res) => {
-  const username = req.session.username;
+// app.get("/trayItemCount", async (req, res) => {
+//   const username = req.session.username;
 
-  const userCollection = database.db(mongodb_database).collection("users");
+//   const userCollection = database.db(mongodb_database).collection("users");
 
-  try {
-    const user = await userCollection.findOne({ username: username });
-    const trayItemCount = user.trayItems.length;
-    res.json({ trayItemCount: trayItemCount });
-  } catch (error) {
-    console.error(error);
-    res.json({ trayItemCount: 0 });
-  }
-});
+//   try {
+//     const user = await userCollection.findOne({ username: username });
+//     const trayItemCount = user.trayItems.length;
+//     res.json({ trayItemCount: trayItemCount });
+//   } catch (error) {
+//     console.error(error);
+//     res.json({ trayItemCount: 0 });
+//   }
+// });
 
 app.get("/mytray", async (req, res) => {
   const username = req.session.username;
